b67653f8f2264c471f86916c0c56a0006b30671c
Veda H Mata <hmata.veda@gmail.com>
2022-04-08 15:29:50 -0400

    CRA

---
+++ react/CRA/firstapp/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello Dojo!</h1>
+      <p className="h2">Things I need to do</p>
+      <ul>
+        <li>Learn react</li>
+        <li>Climb Mt Everext</li>
+        <li>Run Marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
